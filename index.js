e1b38c9f918ebc680a4dd10da6113eac79446204
Ahmed1 <dooubleeee.a@gmail.com>
2025-06-11 01:57:06 +0200

    feat: define password character sets and base config in JS

---
+++ index.js
@@ -0,0 +1,8 @@
+const passwordBox = documentGetElementbyId("password")
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '?', '/', '\\', '|', '~'];
+
