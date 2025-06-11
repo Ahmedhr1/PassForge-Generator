10c4e60dd36c104047f32b09db435945d35fe88c
Ahmed1 <dooubleeee.a@gmail.com>
2025-06-11 02:07:58 +0200

    feat: implement initial password generation logic using randomized character sampling across defined pools

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '?', '/', '\\', '|', '~'];
 
+function generatepass(){
+
+  
+
+let password = "";
+
+password += upperCase[Math.floor(Math.random() * upperCase.length)]
+
+password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+
+password += numbe[Math.floor(Math.random() * number.length)]
+
+password += Symbol[Math.floor(Math.random() * Symbol.length)]
+
+  
+
+}
